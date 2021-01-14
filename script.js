34f0a1163a5f22586029a6fddd28fcaf71d0c3da
Jefferson Augusto Ribeiro <jeffersonari@yahoo.com.br>
2021-01-13 21:41:37 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcular() {
+    var carne = document.getElementById ('carne').value;
+    var refri = document.getElementById ('refri').value;
+    var cerveja = document.getElementById ('cerveja').value;
+    var acomp = document.getElementById ('acomp').value;
+    var nM = document.getElementById ('nMulheres').value;
+    var nH = document.getElementById ('nHomens').value;
+    var nC = document.getElementById ('nCriancas').value;
+   
+
+    var resul = document.getElementById ('res').value;
+}
